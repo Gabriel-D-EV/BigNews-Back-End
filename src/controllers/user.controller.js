b317a588f00bad68c9bbfd3d97edc4fbfdeecffe
Gabriel Silva <gabriel.silva_dev@outlook.com>
2023-12-24 13:44:58 -0300

    att
    criação do update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -54,4 +54,36 @@ const findById = async (req, res) => {
     res.send(user)
 };
 
-module.exports = {create, findAll, findById};
+const update = async (req, res) => {
+    const {name, username, email, password, avatar} = req.body;
+
+    if (!name && !username && !email && !password && !avatar) {
+        res.status(400).send({message: " Preencha algum campo para atualizar."})
+    }
+
+    const id = req.params.id;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).send({message: "ID Inválido!"});
+    }
+
+    const user = await userService.findByIdService(id)
+
+    if(!user) {
+        return res.status(400).send({message: "Não há usuarios"});
+    }
+
+    await userService.updateService(
+        id,
+        name,
+        username,
+        email,
+        password,
+        avatar
+    );
+
+    res.send({message: "Usuario atualizado com sucesso!!"});
+
+};
+
+module.exports = {create, findAll, findById, update};
--- src/services/user.services.js
+++ src/services/user.services.js
@@ -6,6 +6,15 @@ const findAllService = () => User.find();
 
 const findByIdService = (id) => User.findById(id)
 
+const updateService = (
+    id,
+    name,
+    username,
+    email,
+    password,
+    avatar) => User.findOneAndUpdate({_id: id},{ name, username, email, password, avatar }
+    );
+
 module.exports = {
-    createService, findAllService, findByIdService
+    createService, findAllService, findByIdService, updateService,
 }
