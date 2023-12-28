20023475552ccf1796a9dd6d382f98e0627cff83
Gabriel Silva <gabriel.silva_dev@outlook.com>
2023-12-28 01:04:31 -0300

    att
    criptografando a senha do usuario com bcryptjs

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const UserSchema = new mongoose.Schema({
     name: {type: String, required: true},
@@ -6,6 +7,12 @@ const UserSchema = new mongoose.Schema({
     email: {type: String, required: true, unique: true, lowercase: true},
     password: {type: String, required: true, select: false},
     avatar: {type: String, required: true},
+});
+
+//Criptografia da senha
+UserSchema.pre('save', async function (next) {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
 })
 
 const User = mongoose.model("User", UserSchema);
