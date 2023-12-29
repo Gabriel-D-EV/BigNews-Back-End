482c5fd4a930fc04b021ab35e6a0bdf2841cb383
Gabriel Silva <gabriel.silva_dev@outlook.com>
2023-12-29 19:42:39 -0300

    att variaveis de ambiente
    adicionando variaveis de ambiente... dotenv( .env)

--- src/database/db.js
+++ src/database/db.js
@@ -1,7 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDatabase = () => {
-    mongoose.connect("mongodb+srv://root:root@gdev.rbyghrq.mongodb.net/?retryWrites=true&w=majority",{useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log("MongoDB Atlas conectado!")).catch((error) => console.log(error));
-}
+  mongoose
+    .connect(process.env.MONGODB_URI,
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => console.log("MongoDB Atlas conectado!"))
+    .catch((error) => console.log(error));
+};
 
 export default connectDatabase;
