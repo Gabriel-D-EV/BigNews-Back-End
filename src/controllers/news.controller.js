820fda77911206ba15de412854661491c9140f54
Gabriel Silva <gabriel.silva_dev@outlook.com>
2023-12-31 19:59:02 -0300

    att rotas news

---
+++ src/controllers/news.controller.js
@@ -0,0 +1,33 @@
+import { createService, findAllService} from "../services/news.services.js";
+
+const create = async (req, res) => {
+    try {
+        const {title, text, banner} = req.body;
+
+        if (!title ||!text ||!banner) {
+            res.status(400).send({message: " Preencha todos os campos."});
+        }
+
+        await createService ({
+            title,
+            text,
+            banner,
+            user: {_id: "658cf27a4b19ac353dec4fb5"},
+        })
+
+        res.send(201);
+    } catch (err) {
+        res.status(500).send({message: err.message})
+    }
+}
+
+const findAll = async (req, res) => {
+    const news = await findAllService();
+    if(news.length === 0) {
+        return res.status(400).send({message: "Não há Notícias registradas!"});
+    }
+    
+    res.send(news);
+}
+
+export {create, findAll};
