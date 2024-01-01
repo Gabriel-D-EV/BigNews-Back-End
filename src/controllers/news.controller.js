a61c8dd9e534dc1e394b9cc3d24c0b0bdb4bc019
Gabriel Silva <gabriel.silva_dev@outlook.com>
2024-01-01 16:06:36 -0300

    att
    da dando erro no authorization ... aparece indefinido em vez de Bearer token

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,33 +1,51 @@
-import { createService, findAllService} from "../services/news.services.js";
+import { createService, findAllService } from "../services/news.services.js";
 
 const create = async (req, res) => {
-    try {
-        const {title, text, banner} = req.body;
+  try {
+    const { authorizations } = req.headers;
+    console.log(authorizations);
 
-        if (!title ||!text ||!banner) {
-            res.status(400).send({message: " Preencha todos os campos."});
-        }
+    const parts = authorizations.split(" ");
+    const [schema, token] = parts;
 
-        await createService ({
-            title,
-            text,
-            banner,
-            user: {_id: "658cf27a4b19ac353dec4fb5"},
-        })
-
-        res.send(201);
-    } catch (err) {
-        res.status(500).send({message: err.message})
+    if (parts.length !== 2){
+        return res.status(401).send({ message: "Você não está autorizado!" });
     }
-}
+
+    if (!authorizations) {
+      return res.status(401).send({ message: "Você não está autorizado!" });
+    }
+
+    if (schema !== "Bearer") {
+      return res.status(401).send({ message: "Você não está autorizado!" });
+    }
+
+    const { title, text, banner } = req.body;
+
+    if (!title || !text || !banner) {
+      res.status(400).send({ message: " Preencha todos os campos." });
+    }
+
+    await createService({
+      title,
+      text,
+      banner,
+      user: { _id: "658cf27a4b19ac353dec4fb5" },
+    });
+
+    res.send(201);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
 
 const findAll = async (req, res) => {
-    const news = await findAllService();
-    if(news.length === 0) {
-        return res.status(400).send({message: "Não há Notícias registradas!"});
-    }
-    
-    res.send(news);
-}
+  const news = await findAllService();
+  if (news.length === 0) {
+    return res.status(400).send({ message: "Não há Notícias registradas!" });
+  }
 
-export {create, findAll};
+  res.send(news);
+};
+
+export { create, findAll };
