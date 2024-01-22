5e4cb3b885b3067658172df8504ee8ffd257f428
Gabriel Silva <gabriel.silva_dev@outlook.com>
2024-01-22 20:05:25 -0300

    att liberação dos cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDatabase from "./database/db.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
@@ -13,21 +14,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 connectDatabase()
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
 app.use(express.json());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
 app.use("/news", newsRoute);
 app.use("/doc", swaggerRoute);
-//ROTA - 
-//Method HTTP(CRUD)
 
-  //GET - pega
-  //POST - cria
-  //PUT - altera full
-  //PATCH - altera parte
-  //DELETE - apaga
-
-//Nome - um indentificador da rota
-//Function | => - Responsavel por executar algun comando
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
