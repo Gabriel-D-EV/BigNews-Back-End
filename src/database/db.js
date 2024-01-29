9eeaa0d7484ac9f940cfcd063efc327eb3743967
Gabriel Silva <gabriel.silva_dev@outlook.com>
2024-01-29 04:14:53 -0300

    att

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDatabase = () => {
+  console.log("Conectando Banco de dados...");
   mongoose
     .connect(process.env.MONGODB_URI,
       { useNewUrlParser: true, useUnifiedTopology: true }
