7ae818d1353c974f1839c96fbf5eea0670d2e7f4
Gabriel Silva <gabriel.silva_dev@outlook.com>
2024-04-07 21:25:36 -0300

    att

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
 app.use("/news", newsRoute);
-app.use("/docs", swaggerRoute);
+app.use("/", swaggerRoute);
 
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
